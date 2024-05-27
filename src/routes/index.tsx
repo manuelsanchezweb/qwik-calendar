32c4f468ecaefa2d08b7a9722d0a8dbd5ae27322
manuelsanchez2 <manusansan22@gmail.com>
2024-05-27 18:53:39 +0200

    Remove old code

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,10 @@
 import { component$, useSignal, useVisibleTask$ } from '@builder.io/qwik'
 import type { DocumentHead } from '@builder.io/qwik-city'
 import { Calendar } from '~/components/calendar'
-import type { User } from '@supabase/supabase-js'
-import { supabase } from '~/lib/db'
-import { AuthForm } from '~/components/auth'
-import { Navigation } from '~/components/navigation'
 import { LoadingScreen } from '~/components/loading-screen/loading-screen'
 import { IconManager } from '~/icons/icon-manager'
+import { IS_LOADING_FROM_BEGINNING } from '~/config'
+import { FAKE_DATA } from '~/data'
 
 const VIEWS = {
   CALENDAR: 'CALENDAR',
@@ -57,103 +55,120 @@ function CurrentMonthAndYear() {
 }
 
 export default component$(() => {
-  const userSignal = useSignal<User | null>()
-  const isLoading = useSignal(true)
+  const isLoading = useSignal(IS_LOADING_FROM_BEGINNING)
   const selectedView = useSignal<ViewKeys>(VIEWS.CALENDAR)
 
   useVisibleTask$(() => {
-    supabase.auth.getSession().then(({ data: { session } }) => {
-      userSignal.value = session?.user ?? null
-    })
+    // supabase.auth.getSession().then(({ data: { session } }) => {
+    //   userSignal.value = session?.user ?? null
+    // })
 
-    const {
-      data: { subscription: authListener },
-    } = supabase.auth.onAuthStateChange((_, session) => {
-      const currentUser = session?.user
-      userSignal.value = currentUser ?? null
+    setTimeout(() => {
       isLoading.value = false
-    })
+    }, 2000)
 
-    return () => {
-      authListener?.unsubscribe()
-    }
+    // const {
+    //   data: { subscription: authListener },
+    // } = supabase.auth.onAuthStateChange((_, session) => {
+    //   const currentUser = session?.user
+    //   userSignal.value = currentUser ?? null
+    //   isLoading.value = false
+    // })
+
+    // return () => {
+    //   authListener?.unsubscribe()
+    // }
   })
 
   if (isLoading.value) return <LoadingScreen />
 
-  const date = new Date()
-  console.log(date)
-
   return (
     <>
-      {userSignal.value ? (
-        <>
-          <Navigation userSignal={userSignal.value} />
-          {/* Active Date + Today Button */}
-          <div class="flex justify-between items-center">
-            <div class="flex items-center gap-2">
-              <div class="text-primary text-8xl">{CurrentDay()}</div>
-              <div class="flex flex-col text-3xl">
-                <span>{DayName()}</span>
-                <span>{CurrentMonthAndYear()}</span>
-              </div>
+      <>
+        {/* <Navigation userSignal={userSignal.value} /> */}
+        {/* Active Date + Today Button */}
+        <div class="flex justify-between items-center">
+          <div class="flex items-center gap-2">
+            <div class="text-primary text-8xl">{CurrentDay()}</div>
+            <div class="flex flex-col text-3xl">
+              <span>{DayName()}</span>
+              <span>{CurrentMonthAndYear()}</span>
             </div>
-            <button class="btn" onClick$={() => alert('Hoy')}>
-              Today
-            </button>
           </div>
-
-          {/* Filter Buttons + Add Button  */}
-          <div class="flex justify-between items-center bg-grayBrandLight py-6 px-4 rounded-md my-6">
-            <div class="flex gap-2 items-center">
-              <button
-                onClick$={() => (selectedView.value = VIEWS.CALENDAR)}
-                class="transition-transform hover:scale-105 focus:scale-105"
-              >
-                {selectedView.value === VIEWS.CALENDAR ? (
-                  <IconManager
-                    icon="calendar-fill"
-                    classCustom="w-12 h-auto mb-1"
-                  />
-                ) : (
-                  <IconManager icon="calendar" classCustom="w-12 h-auto mb-1" />
-                )}
-              </button>
-              <button
-                onClick$={() => (selectedView.value = VIEWS.LIST)}
-                class="transition-transform hover:scale-105 focus:scale-105"
-              >
-                {selectedView.value === VIEWS.LIST ? (
-                  <IconManager
-                    icon="list-fill"
-                    classCustom="w-12 h-auto mb-1"
-                  />
-                ) : (
-                  <IconManager icon="list" classCustom="w-12 h-auto mb-1" />
-                )}
-              </button>
-            </div>
-
-            <button class="transition-transform hover:scale-105 focus:scale-105">
-              {' '}
-              <IconManager icon="add" classCustom="w-12 h-auto" />
-            </button>
-          </div>
-
-          <span>Vista seleccionada: {selectedView.value}</span>
-          {selectedView.value === VIEWS.CALENDAR ? (
-            <Calendar />
-          ) : (
-            <div class="flex flex-col gap-4">
-              <p>sdasd</p>
-            </div>
-          )}
-        </>
-      ) : (
-        <div class="min-w-full flex items-center justify-center">
-          <AuthForm />
+          <button class="btn" onClick$={() => alert('Hoy')}>
+            Today
+          </button>
         </div>
-      )}
+
+        {/* Filter Buttons + Add Button  */}
+        <div class="flex justify-between items-center bg-grayBrandLight py-6 px-4 rounded-md my-6">
+          <div class="flex gap-2 items-center">
+            <button
+              onClick$={() => (selectedView.value = VIEWS.CALENDAR)}
+              class="transition-transform hover:scale-105 focus:scale-105"
+            >
+              {selectedView.value === VIEWS.CALENDAR ? (
+                <IconManager
+                  icon="calendar-fill"
+                  classCustom="w-12 h-auto mb-1"
+                />
+              ) : (
+                <IconManager icon="calendar" classCustom="w-12 h-auto mb-1" />
+              )}
+            </button>
+            <button
+              onClick$={() => (selectedView.value = VIEWS.LIST)}
+              class="transition-transform hover:scale-105 focus:scale-105"
+            >
+              {selectedView.value === VIEWS.LIST ? (
+                <IconManager icon="list-fill" classCustom="w-12 h-auto mb-1" />
+              ) : (
+                <IconManager icon="list" classCustom="w-12 h-auto mb-1" />
+              )}
+            </button>
+          </div>
+
+          <button class="transition-transform hover:scale-105 focus:scale-105">
+            {' '}
+            <IconManager icon="add" classCustom="w-12 h-auto" />
+          </button>
+        </div>
+
+        <span>Selected view: {selectedView.value}</span>
+
+        {selectedView.value === VIEWS.CALENDAR ? (
+          <>
+            <Calendar />
+            <ul class="flex flex-col gap-4">
+              {FAKE_DATA.map((task) => {
+                return (
+                  <>
+                    <li
+                      key={task.id}
+                      class="flex gap-4 bg-green-200 max-w-[600px] flex-col p-4 rounded-md"
+                    >
+                      <p>{task.title}</p>
+                      <p>{task.date}</p>
+                      <p>
+                        {task.timeStart} - {task.timeEnd}
+                      </p>
+                      <p>{task.category}</p>
+
+                      <button onClick$={() => console.log(task)}>
+                        <IconManager icon="edit" classCustom="w-6 h-auto" />
+                      </button>
+                    </li>
+                  </>
+                )
+              })}
+            </ul>
+          </>
+        ) : (
+          <div class="flex flex-col gap-4">
+            <p>Liste</p>
+          </div>
+        )}
+      </>
     </>
   )
 })
@@ -167,3 +182,7 @@ export const head: DocumentHead = {
     },
   ],
 }
+
+// <div class="min-w-full flex items-center justify-center">
+//   <AuthForm />
+// </div>
