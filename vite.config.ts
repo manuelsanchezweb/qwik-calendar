df556f97537614fdfb872ba6697f0eb8bffaf8ec
manuelsanchez2 <manusansan22@gmail.com>
2024-05-30 18:03:14 +0200

    Add latest version

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite';
 import { qwikVite } from '@builder.io/qwik/optimizer';
 import { qwikCity } from '@builder.io/qwik-city/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { qwikPwa } from "@qwikdev/pwa";
 
 export default defineConfig(() => {
   return {
-    plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
+    plugins: [qwikCity(), qwikVite(), tsconfigPaths(), qwikPwa()],
     preview: {
       headers: {
         'Cache-Control': 'public, max-age=600',
