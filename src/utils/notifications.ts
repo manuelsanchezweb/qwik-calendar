bc075ea260d5c0e1dd7acb23fab51f60cee43297
manuelsanchez2 <manusansan22@gmail.com>
2024-07-10 19:26:03 +0200

    Try to fix url?

--- src/utils/notifications.ts
+++ src/utils/notifications.ts
@@ -5,13 +5,16 @@ export const sendPushNotificationToEndpoint = async ({
   title: string
   message: string
 }) => {
-  const response = await fetch('/notifications', {
-    method: 'POST',
-    body: JSON.stringify({
-      title: title,
-      message: message,
-    }),
-  })
+  const response = await fetch(
+    'https://qwik-calendar.vercel.app/notifications',
+    {
+      method: 'POST',
+      body: JSON.stringify({
+        title: title,
+        message: message,
+      }),
+    }
+  )
 
   if (!response.ok) {
     const error = await response.json()
